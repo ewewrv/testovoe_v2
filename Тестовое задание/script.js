ea39e968ca456807832f401d9dbe8379a0205b41
ewewrv <130379406+ewewrv@users.noreply.github.com>
2023-10-30 15:58:01 +0300

    Add files via upload

---
+++ Тестовое задание/script.js
@@ -0,0 +1,13 @@
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+    speed: 600,
+  
+    // Navigation arrows
+    navigation: {
+        nextEl: '.buttonnext',
+        prevEl: '.buttonprev',
+    },
+
+});
